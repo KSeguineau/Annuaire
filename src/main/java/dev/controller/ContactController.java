3bb44178013863ae024cffeb2b6d301694d67fc5
Kévin Seguineau <seguineaukevin@gmail.com>
2021-01-08 16:53:15 +0100

    "fix controller"

--- src/main/java/dev/controller/ContactController.java
+++ src/main/java/dev/controller/ContactController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -64,7 +65,7 @@ public class ContactController {
 	 * Creating a post mapping that create or update a contact in the database
 	 * @return a Contact
 	 */
-	@GetMapping("/contact")
+	@PostMapping("/contact")
 	private Contact createOrUpdate(@RequestBody Contact contact){
 		return contactManager.create(contact);
 	}
